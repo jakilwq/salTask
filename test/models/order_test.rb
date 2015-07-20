24d70fdc2d4ad05f0b51a0f072ae068d8641a2d8
Dan Baumfeld <thethirddan@gmail.com>
2015-07-20 14:43:15 -0700

    simple models/orders tests

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -7,7 +7,14 @@ class OrderTest < ActiveSupport::TestCase
     @order ||= Order.new
   end
 
-  def test_valid
-    assert order.valid?
+  def test_valid_fixure
+    assert orders(:one).valid?
+    assert_equal 1, orders(:one).stripe_id
   end
+
+  def test_order_instance
+    assert_instance_of Order, order
+  end
+
+  
 end
